bd2f68edbfdf64f466bf048a74be6f5973baf514
Andy Teucher <andy.teucher@gmail.com>
2023-11-14 20:38:25 -0800

    Update course zip, add info to intro slides

--- make-course-zip.R
+++ make-course-zip.R
@@ -1,6 +1,9 @@
 # This makes a zip file in the current directory of the files the learners
 # will need for the course. They can download it and extract it directly,
-# or use usethis::use_course("https://tinyurl.com/3dmxtxaz").
+# or use:
+# usethis::use_course(
+#   "https://github.com/ninoxconsulting/r-telemetry-workshop-nov-2023/raw/main/r-telemetry-workshop.zip"
+# )
 # Full url: https://github.com/ninoxconsulting/r-telemetry-workshop-nov-2023/raw/main/r-telemetry-workshop.zip
 
 d <- withr::local_tempdir()
@@ -13,7 +16,7 @@ dir.create(file.path(projdir, "clean_data"))
 file.copy("raw_data", projdir, recursive = TRUE)
 
 # Remove unnecessary files and dirs made by create_project()
-unlink(file.path(projdir, c("R", ".gitignore")), recursive = TRUE)
+unlink(file.path(projdir, c("R", "raw_data/*.R", ".gitignore")), recursive = TRUE)
 
 outdir <- getwd()
 
